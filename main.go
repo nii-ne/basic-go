8896ac76afaf912247fbe297c7a3b296011f81d3
Artit Attasiri <artit.atta@gmail.com>
2019-06-01 14:23:50 +0700

    API Web Service with gorilla

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	fmt.Println("Hello world")
+	mux := mux.NewRouter()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/profile/{name}", profile)
+
+	fmt.Println("Starting...")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"Welcome to Basic GOLANG"}`))
+}
+func profile(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{"message":"My name is %s"}`, name)
 }
